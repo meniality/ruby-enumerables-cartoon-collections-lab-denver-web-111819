3f1f9bd6a10306183c35bb89e1148c25883097be
patrick hoehn <patrh_99@hotmail.com>
2019-11-13 23:31:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(array_of_calls)
   array_of_calls.map {|n| n.capitalize}
-  array_of_calls.each do |
+  array_of_calls.each do |n| 
 end
 
 def long_planeteer_calls# code an argument here
